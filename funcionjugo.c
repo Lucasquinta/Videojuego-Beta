99b531af909612587c23255fb02d23e81767849f
Lucasquinta <132944976+Lucasquinta@users.noreply.github.com>
2024-06-30 19:41:33 -0300

    Create funcionjugo.c

---
+++ funcionjugo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "libreria.h"
+#define RESET   "\033[0m"
+#define GREEN   "\033[1;32m"
+
+//Creo la funcion que contenera los beneficios del objeto del jugo y llamo al struct con sus respectivas variables
+void jugo(struct xd* x) {
+    //Agrego un puntero con el struct que nos indica que al utilizar el o    bjeto del jugo se le sumara una vida al usuario
+    x->jugadorv += 1;
+    printf(GREEN"Has usado el jugo. Recuperas una vida. Te quedan %d vidas\n"RESET, x->jugadorv);
+}

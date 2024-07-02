e787952b425ef19674aec04d77d5e4cf1607e884
Maximo Gonzalez <136939649+HamRaw10@users.noreply.github.com>
2024-07-02 13:53:29 -0300

    Update funcionlupa.c

--- funcionlupa.c
+++ funcionlupa.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
+#include "colores.h"
 #include "libreria.h"
-#define GREEN   "\033[1;32m"
-#define YELLOW  "\033[1;33m"
-#define RESET   "\033[0m"
 
 //Creamos la funcion de la lupa, donde le agregaremos el struct con sus respectivas variables y agregaremos un puntero
 void lupa(struct xd* x) {

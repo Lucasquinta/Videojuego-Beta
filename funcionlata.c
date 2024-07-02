c9418843158d6cf6bc448cf03372f5200a7a63b0
Maximo Gonzalez <136939649+HamRaw10@users.noreply.github.com>
2024-07-02 13:53:13 -0300

    Update funcionlata.c

--- funcionlata.c
+++ funcionlata.c
@@ -2,15 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include "colores.h"
 #include "libreria.h"
-#define YELLOW  "\033[1;33m"
-#define RED     "\033[1;31m"
-#define BLUE    "\033[1;34m"
-#define RESET   "\033[0m"
-#define GREEN   "\033[1;32m"
-#define MAGENTA "\033[1;35m"
-#define CYAN    "\033[1;36m"
-#define WHITE   "\033[1;37m"
 
 //Creo la funcion de la lata
 void lata(){
@@ -20,12 +13,12 @@ void lata(){
    x.bala = rand() % 2;
   //Creamos en un if que si la bala es 1, sacaremos una bala verdadera
   if(x.bala == 1){
-    printf("Has sacado una bala verdadera\n");
+    printf("A salido bala verdadera\n");
     x.bala = rand() % 2;
   }
   //De lo contrario crearemos otro if que nos indica que si la bala es 0,     sacaremos una bala falsa  
   else if(x.bala == 0){
-    printf("Has sacado una bala falsa\n");
+    printf("A salido una bala falsa\n");
     x.bala = rand() % 2;
   }
 }
